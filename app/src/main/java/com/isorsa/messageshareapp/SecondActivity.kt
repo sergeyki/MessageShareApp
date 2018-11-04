d00ea4e7762cf8193d9967892759e10f40b8a4d5
Sergey Kim <sergey.kim@valuecode.com>
2018-11-05 00:30:29 +0200

    Section two, video three

--- app/src/main/java/com/isorsa/messageshareapp/SecondActivity.kt
+++ app/src/main/java/com/isorsa/messageshareapp/SecondActivity.kt
@@ -2,10 +2,19 @@ package com.isorsa.messageshareapp
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        val bundle : Bundle? = intent.extras
+        val message = bundle!!.getString("user_message")
+
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
+        textUserMessage.text = message
     }
 }
